11dd5c4da5a8a28adea8a8ecc61795598832cfcd
yihong8803 <yihong8803@gmail.com>
2025-03-26 23:02:02 +0800

    Animation Rotate

--- lib/Day6_Animation/animtion2.dart
+++ lib/Day6_Animation/animtion2.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:lottie/lottie.dart';
 import 'package:two_month_flutter/Day6_Animation/lottieAnimation.dart';
 import 'package:two_month_flutter/Day6_Animation/lottieAnimation2.dart';
+import 'dart:math';
 
 class ImplicitAnimation2 extends StatefulWidget {
   const ImplicitAnimation2({super.key});
@@ -12,13 +13,26 @@ class ImplicitAnimation2 extends StatefulWidget {
 
 class _ImplicitAnimation2State extends State<ImplicitAnimation2>
     with TickerProviderStateMixin {
-  bool isExpanded = false;
+  late final AnimationController _controller = AnimationController(
+    //vsync only available with TickerProviderStateMixin
+    vsync: this,
+    duration: const Duration(seconds: 10),
+  )..repeat();
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+    _controller.dispose();
+  }
+
   bool selected = false;
+  double opacityLevel = 1.0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("AnimatedText"),
+        title: Text("AnimatedText & Builder"),
         actions: [
           IconButton(
               onPressed: () {
@@ -32,22 +46,51 @@ class _ImplicitAnimation2State extends State<ImplicitAnimation2>
         ],
       ),
       body: Center(
-        child: GestureDetector(
-          onTap: () {
-            setState(() {
-              selected = !selected;
-            });
-          },
-          child: AnimatedDefaultTextStyle(
-              child: Text("Yihong"),
-              style: selected
-                  ? TextStyle(
-                      fontSize: 30,
-                      color: Theme.of(context).colorScheme.onPrimary)
-                  : TextStyle(
-                      fontSize: 60,
-                      color: Theme.of(context).colorScheme.onSecondary),
-              duration: Duration(seconds: 1)),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            //Changing Opacity of Flutter Logo
+            GestureDetector(
+              onTap: () {
+                setState(() {
+                  opacityLevel = opacityLevel == 0 ? 1.0 : 0.0;
+                });
+              },
+              child: AnimatedOpacity(
+                opacity: opacityLevel,
+                duration: Duration(seconds: 2),
+                child:
+                    Image.asset("assets/img/nerd.jpg", width: 300, height: 300),
+              ),
+            ),
+
+            //Rotating Flutter Logo
+            AnimatedBuilder(
+                animation: _controller,
+                child: Image.asset("assets/img/ghost.png",
+                    width: 100, height: 100),
+                builder: (BuildContext context, Widget? child) {
+                  return Transform.rotate(
+                      angle: _controller.value * 2.0 * pi, child: child);
+                }),
+            GestureDetector(
+              onTap: () {
+                setState(() {
+                  selected = !selected;
+                });
+              },
+              child: AnimatedDefaultTextStyle(
+                  child: Text("Yihong"),
+                  style: selected
+                      ? TextStyle(
+                          fontSize: 30,
+                          color: Theme.of(context).colorScheme.onPrimary)
+                      : TextStyle(
+                          fontSize: 60,
+                          color: Theme.of(context).colorScheme.onSecondary),
+                  duration: Duration(seconds: 1)),
+            ),
+          ],
         ),
       ),
     );
